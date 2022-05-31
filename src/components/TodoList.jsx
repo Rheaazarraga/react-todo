67a40af7dc4930242566b974aa01483fadea47f3
Rhea <rheaazarraga@gmail.com>
2022-05-30 17:06:56 -0700

    pass down todos data into TodoList component and give access to todos within the TodoList component by passing it as an arg

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Todo from './Todo';
 
-const TodoList = () => {
+const TodoList = ({todos}) => {
+  console.log('here is a todo item', todos)
   return (
     <div className="todo-container">
       <ul className="todo-list">
