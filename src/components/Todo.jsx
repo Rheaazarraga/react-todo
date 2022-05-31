e97e1afb5271e7a9d687be8077475157ad4d0f0c
Rhea <rheaazarraga@gmail.com>
2022-05-30 18:13:37 -0700

    add logic based on the state, if the todo item is completed, toggle styling with line through

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -27,12 +27,12 @@ const Todo = ({text, todo, todos, setTodos}) => {
       return item;
     })
     );
-  }
+  };
 
 
   return (
     <div className="todo">
-      <li className='todo-item'>{text}</li>
+      <li className={`todo-item ${todo.completed ? 'completed' : ''}`}>{text}</li>
     <button onClick={completeHandler} className='complete-btn'><i className='fas fa-check'></i></button>
     <button onClick={deleteHandler} className='delete-btn'><i className='fas fa-trash'></i></button>
     </div>
