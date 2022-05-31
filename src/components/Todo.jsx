0461b08af91099c8eadb3cd19026c1b1cd45500e
Rhea <rheaazarraga@gmail.com>
2022-05-30 17:52:45 -0700

    add todo prop to todolist component, add event handler to handle deleting an item from the todo list

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
-const Todo = ({text, todos, setTodos}) => {
+const Todo = ({text, todo, todos, setTodos}) => {
 
   // add events
-  
+  const deleteHandler = (e) => {
+
+    // get all current todos, filter the state out, find an element that matches the element clicked, if it matches the one from the state, delete it
+    setTodos(todos.filter((el) => el.id !== todo.id));
+    console.log(todo);
+  };
   return (
     <div className="todo">
       <li className='todo-item'>{text}</li>
     <button className='complete-btn'><i className='fas fa-check'></i></button>
-    <button className='delete-btn'><i className='fas fa-trash'></i></button>
+    <button onClick={deleteHandler} className='delete-btn'><i className='fas fa-trash'></i></button>
     </div>
   );
 }
