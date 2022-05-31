f26228e4016baee0edcf4173a05adb742c0cd412
Rhea <rheaazarraga@gmail.com>
2022-05-30 17:01:57 -0700

    rename button

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,7 +5,7 @@ const Todo = () => {
     <div className="todo">
       <li className='todo-item'>TEST</li>
     <button className='complete-btn'><i className='fas fa-check'></i></button>
-    <button className='trash-btn'><i className='fas fa-trash'></i></button>
+    <button className='delete-btn'><i className='fas fa-trash'></i></button>
     </div>
   );
 }
