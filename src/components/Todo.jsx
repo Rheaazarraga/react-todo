146278893c94be097bb7d4d8e9decdf8a82da046
Rhea <rheaazarraga@gmail.com>
2022-05-30 18:05:13 -0700

    create handler with logic for completed todo's

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,16 +3,37 @@ import React from 'react';
 const Todo = ({text, todo, todos, setTodos}) => {
 
   // add events
+
+  // delete an item from the list
   const deleteHandler = (e) => {
 
     // get all current todos, filter the state out, find an element that matches the element clicked, if it matches the one from the state, delete it
-    setTodos(todos.filter((el) => el.id !== todo.id));
+    setTodos(todos.filter((item) => item.id !== todo.id));
     console.log(todo);
   };
+
+  // mark an item as completed
+  const completeHandler = (e) => {
+    // go over each item in the state
+    setTodos(todos.map(item => {
+      // check if the items id matches the todo id 
+      if(item.id === todo.id) {
+        return {
+          // use spread to add all other properties and only modify the completed property
+          ...item, completed: !item.completed
+        }
+      }
+      // if it doesn't match, return whatever it was
+      return item;
+    })
+    );
+  }
+
+
   return (
     <div className="todo">
       <li className='todo-item'>{text}</li>
-    <button className='complete-btn'><i className='fas fa-check'></i></button>
+    <button onClick={completeHandler} className='complete-btn'><i className='fas fa-check'></i></button>
     <button onClick={deleteHandler} className='delete-btn'><i className='fas fa-trash'></i></button>
     </div>
   );
